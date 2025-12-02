7fb5ceecd38ad20b9856432c2cf1b361f7373530
ellen <setumasters@gmail.com>
2025-12-02 09:30:13 +0000

    week 5 lsb up to tables and data

--- src/main/kotlin/ie/setu/App.kt
+++ src/main/kotlin/ie/setu/App.kt
@@ -1,9 +1,12 @@
 package ie.setu
 
+import ie.setu.config.DbConfig
 import ie.setu.config.JavalinConfig
 
 fun main() {
 
+    DbConfig().getDbConnection()
     JavalinConfig().startJavalinService()
 
 }
+
